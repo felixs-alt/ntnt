61f8f19738724d5de1636085cb480ea2810f7aa7
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2023-11-16 13:36:01 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 cd client
 npm install
+npm install date-fns
+npm install axios
+npm install bcryptjs
+npm install dotenv
+npm install express
+npm install express-session
+npm install if-env
+npm install mongoose
+npm install passport
+npm install passport-local
 npm run build
